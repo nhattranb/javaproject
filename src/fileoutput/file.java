44971792839b737805b63280e0dff7a97f521438
Nhat Tran <95455356+nhattranb@users.noreply.github.com>
2022-12-13 15:30:59 +0700

    Update the code manually

--- src/fileoutput/file.java
+++ src/fileoutput/file.java
@@ -1,20 +1,33 @@
 package fileoutput;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.File;
+
 public class file {
- public static void main(String args[]) {
+    public static void main(String args[]) {
         File myObj = new File("users.txt");
+
+        String name, address;
+        int age;
+
         Scanner nmap = new Scanner(System.in);
+
+        PrintWriter pw = new PrintWriter(new FileWriter("src\users.txt", true));
+
         System.out.println("Enter your name");
         String name = nmap.nextLine();
-        Scanner owasp = new Scanner(System.in);
-        System.out.println("Enter your address");
-        String address = owasp.nextLine();
-        Scanner zap = new Scanner(System.in);
-        System.out.println("Enter your age");
-        String name = zap.nextLine();
-        PrintWriter pw = null;
-        try {
 
-        }
+        System.out.println("Enter your address");
+        String address = nmap.nextLine();
+
+        System.out.println("Enter your age");
+        int age = nmap.nextInt();
+
+        pw.println(name + "," + address + "," + age);
+
+        pw.flush();
+        pw.close();
     }
 }
