fe69bd3c32cfd5a6e3246f3d123d77af76d2ffee
Nhat Tran <95455356+nhattranb@users.noreply.github.com>
2022-12-13 14:55:25 +0700

    Update file.java
    update the system

--- src/fileoutput/file.java
+++ src/fileoutput/file.java
@@ -1,2 +1,20 @@
-package fileoutput;public class file {
+package fileoutput;
+
+public class file {
+ public static void main(String args[]) {
+        File myObj = new File("users.txt");
+        Scanner nmap = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = nmap.nextLine();
+        Scanner owasp = new Scanner(System.in);
+        System.out.println("Enter your address");
+        String address = owasp.nextLine();
+        Scanner zap = new Scanner(System.in);
+        System.out.println("Enter your age");
+        String name = zap.nextLine();
+        PrintWriter pw = null;
+        try {
+
+        }
+    }
 }
